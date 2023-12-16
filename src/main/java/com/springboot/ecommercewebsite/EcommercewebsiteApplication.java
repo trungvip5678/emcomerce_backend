792fb9ed3120487df668e7fe91df9b1d35772647
a39006 <vuvantrung002@gmail.com>
2023-12-16 22:24:05 +0700

    initial project

---
+++ src/main/java/com/springboot/ecommercewebsite/EcommercewebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ecommercewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercewebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercewebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/ecommercewebsite/EcommercewebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ecommercewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercewebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
