2167ec8f3f4ee7a55ad30e6c0002ab64fe617e3a
a39006 <vuvantrung002@gmail.com>
2023-12-18 15:11:25 +0700

    Security and jwt config

---
+++ src/main/java/com/springboot/ecommercewebsite/security/SpringSecurityConfig.java
@@ -0,0 +1,77 @@
+package com.springboot.ecommercewebsite.security;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class SpringSecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/api/**").authenticated()
+                                .anyRequest().permitAll()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(), BasicAuthenticationFilter.class)  // Replace with your actual filter
+                .csrf().disable()
+                .cors().configurationSource(request -> {
+                    CorsConfiguration cfg = new CorsConfiguration();
+                    cfg.setAllowedOrigins(Arrays.asList(
+                            "http://localhost:3000",
+                            "http://localhost:4200"
+                    ));
+                    cfg.setAllowedMethods(Collections.singletonList("*"));
+                    cfg.setAllowCredentials(true);
+                    cfg.setAllowedHeaders(Collections.singletonList("*"));
+                    cfg.setExposedHeaders(Arrays.asList("Authorization"));
+                    cfg.setMaxAge(3600L);
+                    return cfg;
+                })
+                .and()
+                .httpBasic()
+                .and()
+                .formLogin();
+
+        return http.build();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
