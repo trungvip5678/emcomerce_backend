9a783dcc0110b7047c647c7aec71a71906158bfc
a39006 <vuvantrung002@gmail.com>
2023-12-20 16:38:58 +0700

    add model for rating, review

--- src/main/java/com/springboot/ecommercewebsite/model/Rating.java
+++ src/main/java/com/springboot/ecommercewebsite/model/Rating.java
@@ -2,13 +2,20 @@ package com.springboot.ecommercewebsite.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "ratings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Rating {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/springboot/ecommercewebsite/model/Review.java
+++ src/main/java/com/springboot/ecommercewebsite/model/Review.java
@@ -3,13 +3,17 @@ package com.springboot.ecommercewebsite.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Table(name = "reviews")
 public class Review {
     @Id
