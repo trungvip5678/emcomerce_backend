3f5aca26e0584539b5d3cb12509b73eed825beac
a39006 <vuvantrung002@gmail.com>
2023-12-22 22:06:50 +0700

    fix product Repository

--- src/main/java/com/springboot/ecommercewebsite/repository/ProductRepository.java
+++ src/main/java/com/springboot/ecommercewebsite/repository/ProductRepository.java
@@ -9,8 +9,8 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query("SELECT p FROM Product p " +
-            "WHERE (p.category.name = :category OR :category='')"
+    @Query("SELECT p FROM Product p "+
+            "WHERE (p.category.name = :category OR :category=' ')"
             + "AND ((:minPrice IS NULL AND :maxPrice IS NULL) OR (p.discountedPrice BETWEEN :minPrice AND :maxPrice)) "
             + "AND (:minDiscount IS NULL OR p.discountPercent >= :minDiscount) "
             + "ORDER BY "
@@ -18,8 +18,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             + "CASE WHEN :sort = 'price_high' THEN p.discountedPrice END DESC"
 
         )
-    public List<Product> filterProducts(@Param("category") String category,
+    public List<Product> filterProducts(
+                                        @Param("category") String category,
                                         @Param("minPrice") Integer minPrice,
+                                        @Param("maxPrice") Integer maxPrice,
                                         @Param("minDiscount") Integer minDiscount,
                                         @Param("sort") String sort
                                         );
