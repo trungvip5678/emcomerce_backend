26e7a4cb1d5ba34ab4dac9f9fb3234cfd0feeac5
a39006 <vuvantrung002@gmail.com>
2024-01-08 17:59:12 +0700

    add api response

---
+++ src/main/java/com/springboot/ecommercewebsite/response/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.springboot.ecommercewebsite.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ApiResponse {
+    String message;
+    Boolean Status;
+}
